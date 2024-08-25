27b9dd312deed3ce4d6bd7e546e93c8100993887
dinhkhoi9406 <khoa1232123@gmail.com>
2024-08-26 00:14:02 +0700

    update login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { LoginDTO } from 'src/users/dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async login(loginDTO: LoginDTO): Promise<User> {
+    const user = await this.userService.findOne(loginDTO);
+    const passwordMatched = await bcrypt.compare(
+      loginDTO.password,
+      user.password,
+    );
+    if (passwordMatched) {
+      delete user.password;
+      return user;
+    } else {
+      throw new UnauthorizedException('Password mismatch');
+    }
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { AuthService } from 'src/auth/auth.service';
+import { LoginDTO } from './dto/login.dto';
 
 @Injectable()
 export class UsersService {
@@ -17,6 +19,14 @@ export class UsersService {
     userDTO.password = await bcrypt.hash(userDTO.password, salt);
     const user = await this.userRepository.save(userDTO);
     delete user.password;
+    return user;
+  }
+
+  async findOne(loginDTO: LoginDTO): Promise<User> {
+    const user = await this.userRepository.findOneBy({ email: loginDTO.email });
+    if (!user) {
+      throw new UnauthorizedException('Could not find user');
+    }
     return user;
   }
 }
