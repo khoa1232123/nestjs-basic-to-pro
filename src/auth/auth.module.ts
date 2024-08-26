5f66729f3dbc1b2fbfdedb31b0fdae2514fdfaea
dinhkhoi9406 <khoa1232123@gmail.com>
2024-08-26 22:33:01 +0700

    Authenticate User with Passport JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
+import { authConstants } from './auth.constants';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: authConstants.secret,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,16 @@ import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcryptjs';
 import { LoginDTO } from 'src/users/dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDTO: LoginDTO): Promise<User> {
+  async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
     const user = await this.userService.findOne(loginDTO);
     const passwordMatched = await bcrypt.compare(
       loginDTO.password,
@@ -16,7 +20,12 @@ export class AuthService {
     );
     if (passwordMatched) {
       delete user.password;
-      return user;
+    //   const payload = user;
+        const payload = { email: user.email, sub: user.id };
+
+      return {
+        accessToken: this.jwtService.sign(payload),
+      };
     } else {
       throw new UnauthorizedException('Password mismatch');
     }
